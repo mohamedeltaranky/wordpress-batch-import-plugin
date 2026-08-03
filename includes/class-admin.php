3cac56d310cc53c0fb1c1ed610f12ef39cfd2d84
root <root@srv1869002.hstgr.cloud>
2026-08-03 16:01:15 +0000

    Move admin page to template

--- includes/class-admin.php
+++ includes/class-admin.php
@@ -1,4 +1,4 @@
-<?php
+ن<?php
 
 if (!defined('ABSPATH')) {
     exit;
@@ -26,28 +26,6 @@ class WBIP_Admin
 
     public function render_page()
     {
-        ?>
-        <div class="wrap">
-            <h1>WP Queue Importer</h1>
-
-            <p>Import thousands of URLs using batch processing.</p>
-
-            <textarea
-                id="wbip-links"
-                rows="15"
-                style="width:100%;"
-                placeholder="Paste one URL per line"></textarea>
-
-            <br><br>
-
-            <button
-                id="wbip-start"
-                class="button button-primary">
-                Start Import
-            </button>
-
-            <div id="wbip-progress" style="margin-top:20px;"></div>
-        </div>
-        <?php
+        require WBIP_PATH . 'templates/admin-page.php';
     }
 }
