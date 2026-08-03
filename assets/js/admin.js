95c904300e9ce0b8dbff1666e57370980cf9a9c4
root <root@srv1869002.hstgr.cloud>
2026-08-03 16:10:51 +0000

    Load CSS and JavaScript assets

---
+++ assets/js/admin.js
@@ -0,0 +1,9 @@
+jQuery(function ($) {
+
+    $('#wbip-start').on('click', function () {
+
+        alert('JavaScript Loaded Successfully');
+
+    });
+
+});
--- includes/class-admin.php
+++ includes/class-admin.php
@@ -1,4 +1,4 @@
-ن<?php
+<?php
 
 if (!defined('ABSPATH')) {
     exit;
@@ -9,6 +9,7 @@ class WBIP_Admin
     public function __construct()
     {
         add_action('admin_menu', [$this, 'register_menu']);
+        add_action('admin_enqueue_scripts', [$this, 'enqueue_assets']);
     }
 
     public function register_menu()
@@ -21,6 +22,37 @@ class WBIP_Admin
             [$this, 'render_page'],
             'dashicons-database-import',
             30
+        );
+    }
+
+    public function enqueue_assets($hook)
+    {
+        if ($hook !== 'toplevel_page_wp-queue-importer') {
+            return;
+        }
+
+        wp_enqueue_style(
+            'wbip-admin',
+            WBIP_URL . 'assets/css/admin.css',
+            [],
+            WBIP_VERSION
+        );
+
+        wp_enqueue_script(
+            'wbip-admin',
+            WBIP_URL . 'assets/js/admin.js',
+            ['jquery'],
+            WBIP_VERSION,
+            true
+        );
+
+        wp_localize_script(
+            'wbip-admin',
+            'wbip',
+            [
+                'ajax_url' => admin_url('admin-ajax.php'),
+                'nonce'    => wp_create_nonce('wbip_nonce'),
+            ]
         );
     }
 
