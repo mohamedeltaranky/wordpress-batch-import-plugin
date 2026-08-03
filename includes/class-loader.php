1f4649314a38a9dfd21413fe0d7419cdc90bbd22
mohamedeltaranky <hamadaeltaranky10@gmail.com>
2026-08-03 16:26:22 +0300

    Update class-loader.php

--- includes/class-loader.php
+++ includes/class-loader.php
@@ -9,8 +9,7 @@ class WBIP_Loader
     public static function init()
     {
         require_once WBIP_PATH . 'includes/class-admin.php';
-        require_once WBIP_PATH . 'includes/class-ajax.php';
-        require_once WBIP_PATH . 'includes/class-importer.php';
-        require_once WBIP_PATH . 'includes/class-queue.php';
+
+        new WBIP_Admin();
     }
 }
